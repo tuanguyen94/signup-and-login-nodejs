12ed4b0acb4c32dec642f05fbb4e875286353d7e
Tuanguyen <tuanguyen94@gmail.com>
2016-03-14 22:45:56 +0700

    final change

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,58 @@ var express = require('express');
 var router = express.Router();
 
 var mongodb = require('mongodb');
+var crypto = require('crypto');
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  if(req.cookies.cookieUser == null)
+  {
+    res.cookie('cookieUser',"no one");
+  }
+  var MongoClient = mongodb.MongoClient;
+  var url = 'mongodb://localhost:27017/spiderum';             //duong dan den database
+  MongoClient.connect(url,function(err,db){
+    if(err){
+      console.log("unable to connect to database",err);
+    }
+    else
+    {
+      console.log("connection Established !! :D");
+
+      var collection = db.collection('user');             //chon table
+      collection.find({
+        cookieUser: req.cookies.cookieUser
+      }).toArray(function(err,result){
+        if(err){
+          res.send(err);
+        }
+        else if(result.length){
+          res.render('index', { title: 'Express',user: result[0].username });
+        }
+        else
+        {
+          res.render('index', { title: 'Express',user: "no one" });
+        }
+
+        db.close();
+      });
+    }
+  });
 });
 
-router.get('/thelist',function(req,res){
+router.get('/login',function(req,res){
+  res.render('login',{title : "Login"})
+});
+
+router.post('/login',function(req,res){
+  //connect to database, find and check user
+  //database: spiderum
+  //table: user
+  console.log("you are logning");
   var MongoClient = mongodb.MongoClient;
-  var url = 'mongodb://localhost:27017/test';             //duong dan den database
+  var url = 'mongodb://localhost:27017/spiderum';             //duong dan den database
   MongoClient.connect(url,function(err,db){
     if(err){
       console.log("unable to connect to database",err);
@@ -19,65 +63,106 @@ router.get('/thelist',function(req,res){
       console.log("connection Established !! :D");
 
 
-      var collection = db.collection('test');             //chon table
-      collection.find({}).toArray(function(err,result){
+      var collection = db.collection('user');             //chon table
+      collection.find({
+        username: req.body.username,
+        password: req.body.password
+      }).toArray(function(err,result){
         if(err){
           res.send(err);
         }
-
-
         else if(result.length){
-          res.render('studentList',{ los :result });          //los:list of student
+          //res.cookie('user',req.body.username);
+          //var message = crypto.md5.encrypt('Nội dung cần mã hóa', 'itsasecret123').toString();
+          if(req.body.remember){
+            var exdate = new Date();
+            exdate.setDate(exdate.getDate()+10);
+            res.cookie('cookieUser',crypto.createHash('md5').update(req.body.username).digest('hex'),{expires:exdate});
+            res.redirect('/');
+          }
+          else
+          {
+            res.render('index', { title: 'Express',user: req.body.username });
+          }
+
         }
         else
         {
-          res.send('no document fount in database');
+          res.send('wrong user name or password');
         }
 
         db.close();
       });
-
     }
   });
 });
 
-router.get('/newstudent', function(req,res){
-  res.render('NewStudent',{ title: "new student"});
+router.get('/signup',function(req,res){
+  res.render('signup',{title:"Signup"});
 });
-
-router.get('/newstudent',function(req,res){
-  req.render('NewStudent',{title:"Add Student"});
-});
-router.post('/AddStudent',function(req,res){
+router.post('/signup',function(req,res){
+  //database: spiderum
+  //table: user
   var MongoClient = mongodb.MongoClient;
-  var url = 'mongodb://localhost:27017/test';
-  MongoClient.connect(url,function(err,db){
-    if(err){
-      console.log("Unable to connect to server while add new student!!");
+  var url = 'mongodb://localhost:27017/spiderum';             //duong dan den database
+  MongoClient.connect(url,function(err,db) {
+    if (err) {
+      console.log("unable to connect to database", err);
     }
-    else
-    {
-      console.log("server are already to connect to add new student!!");
-      var collection = db.collection('test');
-      var newStudent = {
-        student: req.body.student,
-        street: req.body.street,
-        city: req.body.city,
-        state: req.body.state,
-        sex: req.body.sex,
-        gpa: req.body.gpa
-      };
-      collection.insert([newStudent],function(err,result){
-        if(err){
-          console.log(err);
+    else {
+      console.log("connection Established !! :D");
+      var collection = db.collection('user');             //chon table
+      collection.find({
+        username: req.body.username
+        //password: req.body.password
+      }).toArray(function (err, result) {
+        if (err) {
+          res.send(err);
         }
-        else
-        {
-          res.redirect("thelist");
+        else if (result.length) {
+          //username da ton tai
+          res.send('your username has already exsist !!!');
+          //los:list of student
         }
-        db.close();
+        else {
+          //username chua trung
+          //them ten nguoi dung vao database
+          var collection = db.collection('user');
+          var newuser = {
+            username: req.body.username,
+            password: req.body.password,
+            email: req.body.email,
+            cookieUser: crypto.createHash('md5').update(req.body.username).digest('hex')
+          };
+          collection.insert([newuser], function (err, result) {
+            if (err) {
+              console.log(err);
+            }
+            var exdate = new Date();
+            exdate.setDate(exdate.getDate()+10);
+            res.cookie('cookieUser',crypto.createHash('md5').update(req.body.username).digest('hex'),{expires:exdate});
+            res.redirect('/');
+          });
+          db.close();
+        }
       });
-    }
+    }       //else {}
   });
 });
+
+function refreshSession(){
+  var temp = req.session.passport; // {user: 1}
+  req.session.regenerate(function(err){
+    //req.session.passport is now undefined
+    req.session.passport = temp;
+    req.session.save(function(err){
+      res.send(200);
+    });
+  });
+}
+router.get('/logout',function(req,res){
+  res.cookie('cookieUser',0);
+  res.render('index',{title: 'Express',user: "no one"})
+});
+
 module.exports = router;
---
+++ routes/login.js
@@ -0,0 +1,4 @@
+/**
+ * Created by Naut on 12-Mar-16.
+ */
+var login = require('')
